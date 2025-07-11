0ffdc5f311eaf23953c217f716365dd7b17e17bd
GaneshRawool0718 <ganesh.rawool@nonstopio.com>
2025-07-11 10:20:21 +0530

    feat(DSA): implement a method to merge two sorted arrays into one sorted array

--- Day19/MergedSort.java
+++ Day19/MergedSort.java
@@ -1,11 +1,17 @@
 package Day19;
 
 public class MergedSort {
+// This class implements a method to merge two sorted arrays into one sorted array. 
+// The first array is modified in place to contain the merged result.
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int mIndex = m - 1;
         int nIndex = n - 1;
         int midStart = m + n - 1 ;
 
+        // This method merges two sorted arrays nums1 and nums2 into nums1.
+        // It assumes nums1 has enough space to hold the elements of nums2.
+        // The merging is done in reverse order to avoid overwriting elements in nums1.
+        // It starts from the end of both arrays and compares the elements,
         while(mIndex >= 0 &&  nIndex >= 0){
             if(nums1[mIndex] > nums2[nIndex]){
                 nums1[midStart] = nums1[mIndex];
@@ -26,16 +32,3 @@ public class MergedSort {
     }
 }
 
-class Client {
-    public static void main(String[] args) {
-        MergedSort mergedSort = new MergedSort();
-        int[] nums1 = {1, 2, 3, 0, 0, 0};
-        int[] nums2 = {2, 5, 6};
-        mergedSort.merge(nums1, 3, nums2, 3);
-        
-        // Print the merged array
-        for (int num : nums1) {
-            System.out.print(num + " ");
-        }
-    }
-}
